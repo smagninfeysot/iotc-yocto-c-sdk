701de1659c6b2ca4e49d495ac67c5763b27a8768
Simon Magnin-Feysot <smagninfeysot@witekio.com>
2024-03-11 16:36:05 +0100

    ci: just test build

--- .github/ci-scripts/build-yocto.sh
+++ .github/ci-scripts/build-yocto.sh
@@ -11,14 +11,17 @@ pip3 install sphinx
 ./setup.py --bsp=01047
 cp -r /src/yocto-layers/meta-iotconnect /src/msc-ldk/sources/
 cp -r /src/yocto-layers/meta-myExampleIotconnectLayer /src/msc-ldk/sources/
-cp -r /src/b0000-internal-pluma-linux-advanced-test-suite/meta-lats/meta-lats-kirkstone/ /src/msc-ldk/sources/
-source sources/yocto.git/oe-init-build-env build/01047
-bitbake-layers add-layer ../../sources/meta-iotconnect/
-bitbake-layers add-layer ../../sources/meta-myExampleIotconnectLayer/
-bitbake-layers add-layer ../../sources/meta-lats-kirkstone/
-echo -e '\nIMAGE_INSTALL += " iotc-c-sdk"' >> conf/local.conf
-echo -e '\nIMAGE_FEATURES += " ssh-server-openssh"' >> conf/local.conf
-echo -e '\nEXTRA_IMAGE_FEATURES += " ptest-pkgs"' >> conf/local.conf
-echo -e '\nDISTRO_FEATURES += " wayland vulkan pam systemd ptest bluetooth"' >> conf/local.conf
-cat conf/local.conf
-bitbake core-image-minimal
+ls -lrth /src
+ls ../b0000-internal-pluma-linux-advanced-test-suite/meta-lats/meta-lats-kirkstone/
+#cp -r /src/b0000-internal-pluma-linux-advanced-test-suite/meta-lats/meta-lats-kirkstone/ /src/msc-ldk/sources/
+#source sources/yocto.git/oe-init-build-env build/01047
+#bitbake-layers add-layer ../../sources/meta-iotconnect/
+#bitbake-layers add-layer ../../sources/meta-myExampleIotconnectLayer/
+#bitbake-layers add-layer ../../sources/meta-lats-kirkstone/
+#echo -e '\nIMAGE_INSTALL += " iotc-c-sdk"' >> conf/local.conf
+#echo -e '\nIMAGE_FEATURES += " ssh-server-openssh"' >> conf/local.conf
+#echo -e '\nEXTRA_IMAGE_FEATURES += " ptest-pkgs"' >> conf/local.conf
+#echo -e '\nDISTRO_FEATURES += " wayland vulkan pam systemd ptest bluetooth"' >> conf/local.conf
+#cat conf/local.conf
+#bitbake core-image-minimal
+#
